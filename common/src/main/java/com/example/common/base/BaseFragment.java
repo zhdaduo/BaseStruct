3561222f1713a405ab74e22e1b1f2f7ed577f524
zhdaduo <1250334290@qq.com>
2017-08-10 21:45:46 +0800

    Fragment : ButterKnife bind view on onViewStateRestored

--- common/src/main/java/com/example/common/base/BaseFragment.java
+++ common/src/main/java/com/example/common/base/BaseFragment.java
@@ -1,5 +1,9 @@
 package com.example.common.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import com.example.common.di.AppComponent;
 import com.example.common.di.GlobalAppComponent;
@@ -12,6 +16,7 @@ import javax.inject.Inject;
 
 public abstract class BaseFragment<P extends IPresenter> extends RxFragment {
 
+  @Nullable
   protected Unbinder unbinder;
 
   @Inject
@@ -19,6 +24,15 @@ public abstract class BaseFragment<P extends IPresenter> extends RxFragment {
 
   protected AppComponent getAppComponent() {
     return GlobalAppComponent.getAppComponent();
+  }
+
+  @Override
+  public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+    super.onViewStateRestored(savedInstanceState);
+    View view = getView();
+    if (view != null) {
+      unbinder = ButterKnife.bind(this, view);
+    }
   }
 
   @Override
